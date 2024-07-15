daddee4f87f68bd2cf2050504b91d880491d684c
ranipatricia <119095924+ranipatricia@users.noreply.github.com>
2024-07-16 05:11:00 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import LandingPage from './komponen/LandingPage';
+import Landing from './Pages/Landing';
+import State from './Pages/State';
+
+
+function App () {
+  return (
+    <>
+      <Routes>
+        <Route path="/" Component={Home}/>
+        <Route path="/landingpage" Component={Landing}/>
+        <Route path="/state" Component={State}/>
+        
+
+       </Routes>
+    </>
+  )
+}
+
+export default App
